8c0a42ab2c945265fb06313fda5b6a55fab6278a
Tim <tbrandle53@gmail.com>
2017-06-01 19:17:44 -0600

    Pushing to Heroku

--- knexfile.js
+++ knexfile.js
@@ -23,37 +23,29 @@ module.exports = {
       directory: './db/seeds/test',
     },
     useNullAsDefault: true,
+  },
+  development: {
+    client: 'pg',
+    connection: 'postgres://localhost/garage_bin',
+    migrations: {
+      directory: './db/migrations',
+    },
+    useNullAsDefault: true,
+    seeds: {
+      directory: './db/seeds/dev',
+    },
+  },
+  production: {
+    client: 'pg',
+    connection: process.env.DATABASE_URL + `?ssl=true`,
+    migrations: {
+      directory: './db/migrations',
+    },
+    useNullAsDefault: true,
+    seeds: {
+      directory: './db/seeds/dev',
+    },
   }
-  // staging: {
-  //   client: 'postgresql',
-  //   connection: {
-  //     database: 'my_db',
-  //     user:     'username',
-  //     password: 'password'
-  //   },
-  //   pool: {
-  //     min: 2,
-  //     max: 10
-  //   },
-  //   migrations: {
-  //     tableName: 'knex_migrations'
-  //   }
-  // },
 
-  // production: {
-  //   client: 'postgresql',
-  //   connection: {
-  //     database: 'my_db',
-  //     user:     'username',
-  //     password: 'password'
-  //   },
-  //   pool: {
-  //     min: 2,
-  //     max: 10
-  //   },
-  //   migrations: {
-  //     tableName: 'knex_migrations'
-  //   }
-  // }
 
 };
